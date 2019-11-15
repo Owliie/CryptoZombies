082c518e51ae18eadce6552a09492c726b8d4f0f
Owliie <sladurcheto5232@abv.bg>
2019-11-16 00:07:06 +0200

    Added a public array of zombies and a create new zombie function

--- zombies.sol
+++ zombies.sol
@@ -5,9 +5,15 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    struct Zombie{
+    struct Zombie {
         string name;
         uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function createZombie(string _name, uint _dna) {
+        zombies.push(Zombie(_name, _dna));
     }
 
 }
